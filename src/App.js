8676b18232c729b53bac1ff88bb0d3e86709a40c
Christopher Coles <cbc1g16@soton.ac.uk>
2019-03-17 10:49:17 +0000

    [Fix] Moved appbar to App.js, fixed the position

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
-import Home from './Components/Home.js'
+import Kidney from './Components/Kidney.js'
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Toolbar from '@material-ui/core/Toolbar';
+
 class App extends Component {
   render() {
     return (
-    <div className="App">
-<label>Simple Stents </label>
-        <Home></Home>
-        </div>
-        
+      <Fragment>
+      
+    
+    <AppBar  position="fixed">
+    <Toolbar>
+              <Button  color="inherit">Home</Button>
+              <Button color="inherit">Kidney</Button>
+              <Button color="inherit">Bladder</Button>
+    </Toolbar>
+    </AppBar>
+
+        <Kidney></Kidney>
+        </Fragment>
 
     );
   }
-}
+} 
 
 export default App;
