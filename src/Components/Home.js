b2a9e3bdecfdfd470262e9063ec5db460ffd107a
Christopher Coles <cbc1g16@soton.ac.uk>
2019-03-20 11:05:34 +0000

    Create Home.js

---
+++ src/Components/Home.js
@@ -0,0 +1,33 @@
+import React, { Component, Fragment } from 'react';
+
+
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          
+         };
+       }
+       
+    render() {
+      const leftstyle = {
+        float: "left",
+        width: "50%",
+      }
+      const rightstyle = {
+        marginTop:"55px",
+        float: "right",
+        width: "50%"
+      }
+    return (
+        
+        <Fragment>
+            
+
+        </Fragment>
+    );
+  }
+}
+
+export default Home;
