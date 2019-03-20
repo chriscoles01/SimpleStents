f1dbc14fd5d1b92a2983e3d9fd51c22339d9aeba
Christopher Coles <cbc1g16@soton.ac.uk>
2019-03-20 11:05:42 +0000

    Update Kidney.js

--- src/Components/Kidney.js
+++ src/Components/Kidney.js
@@ -1,16 +1,14 @@
 import React, { Component, Fragment } from 'react';
 import { VideoScroll } from 'react-video-scroll'
-import IconButton from '@material-ui/core/IconButton';
-import ExpandIcon from '@material-ui/icons/ArrowDropDownCircle'
+// import IconButton from '@material-ui/core/IconButton';
+// import ExpandIcon from '@material-ui/icons/ArrowDropDownCircle'
 import {Stickyroll} from '@stickyroll/stickyroll';
 import first from '../simplestents_images/first.png'
 import second from '../simplestents_images/second.png'
 import third from '../simplestents_images/third.png'
 import fourth from '../simplestents_images/fourth.png'
 import fifth from '../simplestents_images/fifth.png'
-
-
-import Kidneytones from '../simplestents_images/kidneystones.jpg'
+import Fade from 'react-reveal/Fade';
 // const setStyles = (wrapperEl, videoEl, playbackRate) => {
 //   wrapperEl.style.marginTop = `calc(180% - ${Math.floor(videoEl.duration) *
 //     playbackRate +
@@ -31,7 +29,7 @@ class Kidney extends Component {
     constructor(props) {
         super(props);
         this.state = {
-      
+         
          };
        }
        onRefresh(iScrollInstance) {
@@ -47,18 +45,37 @@ class Kidney extends Component {
       }
     getImage = progress =>{
       if (progress < 1/5) {
-        return first
+        return (<Fade id="first" bottom >
+          <img style={{  width: '100%', height: '100%', objectFit: 'contain' }} src={first} alt="" />
+          </Fade>)
       } else if(progress < 2/5){
-        return second
+        return (<Fade id="se" bottom >
+          <img style={{  width: '100%', height: '100%', objectFit: 'contain' }} src={second} alt="" />
+          </Fade>)
       } else if(progress < 3/5){
-        return third
+        return (<Fade id="third" bottom >
+          <img style={{  width: '100%', height: '100%', objectFit: 'contain' }} src={third} alt="" />
+          </Fade>)
       }
       else if(progress < 4/5){
-        return fourth
+        return (<Fade id="fourth" bottom >
+          <img style={{  width: '100%', height: '100%', objectFit: 'contain' }} src={fourth} alt="" />
+          </Fade>)
       }else if(progress <= 1.0){
-        return fifth
+        return (<Fade id="fifth" bottom >
+          <img style={{  width: '100%', height: '100%', objectFit: 'contain' }} src={fifth} alt="" />
+          </Fade>)
       }
     }
+    // getImg = progress => {
+    //   return(
+        
+    //     <Fade bottom >
+    //         <img style={{  width: '100%', height: '100%', objectFit: 'contain' }} src={this.getImage(progress)} alt="" />
+    //         </Fade>
+
+    //   );
+    // }
     render() {
       const leftstyle = {
         paddingTop:"55px",
@@ -100,9 +117,7 @@ class Kidney extends Component {
           </VideoScroll>
             </div>
             <div style={rightstyle}>
-            
-            <img style={{  width: '100%', height: '100%', objectFit: 'contain' }} src={this.getImage(progress)} alt="" />
-             
+            {this.getImage(progress)}
               </div>
             
             </Fragment> 
